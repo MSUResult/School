d4fca7dbd3ee19415b701739708eb61ade7727d1
MSUResult <adarshrajput9040@gmail.com>
2025-11-19 11:43:39 +0530

    first commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -29,7 +29,7 @@ const Contact = () => {
     setResponseMsg("");
 
     try {
-      const response = await fetch("http://localhost:3000/api/contact", {
+      const response = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
